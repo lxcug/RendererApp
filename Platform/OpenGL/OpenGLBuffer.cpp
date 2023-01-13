97932cab26f5cdb79f2e761947ed771b9712ed73
hustLX <1819199531@qq.com>
2023-01-14 00:30:58 +0800

    OpenGL VertexBuffer IndexBuffer

---
+++ Platform/OpenGL/OpenGLBuffer.cpp
@@ -0,0 +1,49 @@
+#include "OpenGLBuffer.h"
+#include "Glad/glad.h"
+
+// --------------------------------------Vertex Buffer--------------------------------------
+
+RendererSpace::OpenGLVertexBuffer::OpenGLVertexBuffer(unsigned int size) {
+    glGenBuffers(1, &m_rendererID);
+    glBindBuffer(GL_ARRAY_BUFFER, m_rendererID);  // bind vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, size, nullptr, GL_DYNAMIC_DRAW);
+}
+
+RendererSpace::OpenGLVertexBuffer::OpenGLVertexBuffer(float* vertices, unsigned size) {  // NOLINT
+    glGenBuffers(1, &m_rendererID);
+    glBindBuffer(GL_ARRAY_BUFFER, m_rendererID);  // bind vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+}
+
+RendererSpace::OpenGLVertexBuffer::~OpenGLVertexBuffer() {
+    glDeleteBuffers(1, &m_rendererID);
+}
+
+void RendererSpace::OpenGLVertexBuffer::bind() const {
+    glBindBuffer(GL_ARRAY_BUFFER, m_rendererID);  // bind OpenGL vertex buffer
+}
+
+void RendererSpace::OpenGLVertexBuffer::unBind() const {
+    glBindBuffer(GL_ARRAY_BUFFER, 0);  // unbind OpenGL vertex buffer
+}
+
+
+// --------------------------------------Index Buffer--------------------------------------
+
+RendererSpace::OpenGLIndexBuffer::OpenGLIndexBuffer(unsigned int *indices, unsigned int count): m_count(count) {
+    glGenBuffers(1, &m_rendererID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_rendererID);  // bind index buffer
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned), indices, GL_STATIC_DRAW);
+}
+
+RendererSpace::OpenGLIndexBuffer::~OpenGLIndexBuffer() {
+    glDeleteBuffers(1, &m_rendererID);
+}
+
+void RendererSpace::OpenGLIndexBuffer::bind() const {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_rendererID);  // bind OpenGL index buffer
+}
+
+void RendererSpace::OpenGLIndexBuffer::unBind() const {
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);  // unbind OpenGL index buffer
+}
---
+++ Platform/OpenGL/OpenGLBuffer.h
@@ -0,0 +1,63 @@
+// @author: lx
+// @time: 2022/10/15
+// @brief: 
+
+#pragma once
+#ifndef GAME_OPENGLBUFFER_H
+#define GAME_OPENGLBUFFER_H
+
+#include "Core/Core.h"
+#include "Buffers/Buffer.h"
+#include "Glad/glad.h"
+
+
+namespace RendererSpace {
+    class OpenGLVertexBuffer : public VertexBuffer {
+    public:
+        OpenGLVertexBuffer() = default;
+        OpenGLVertexBuffer(unsigned size);  // NOLINT
+        OpenGLVertexBuffer(float* vertices, unsigned size);
+        ~OpenGLVertexBuffer() override;
+
+        void bind() const override;
+        void unBind() const override;
+
+        void setData(const void *data, unsigned int size) override {
+            glBindBuffer(GL_ARRAY_BUFFER, m_rendererID);
+            glBufferSubData(GL_ARRAY_BUFFER, 0, size, data);
+        }
+
+        [[nodiscard]] const BufferLayout & getLayout() const override {
+            return m_layout;
+        }
+        void setLayout(const BufferLayout &layout) override {
+            m_layout = layout;
+        }
+
+    private:
+        unsigned m_rendererID;
+        BufferLayout m_layout;
+    };
+
+
+    class OpenGLIndexBuffer : public IndexBuffer {
+    public:
+        OpenGLIndexBuffer() = default;
+        OpenGLIndexBuffer(unsigned* indices, unsigned count);
+        ~OpenGLIndexBuffer() override;
+
+        void bind() const override;
+        void unBind() const override;
+
+        [[nodiscard]] unsigned int getCount() const override {
+            return m_count;
+        }
+
+    private:
+        unsigned m_rendererID;
+        unsigned m_count = 0;
+    };
+}
+
+
+#endif //GAME_OPENGLBUFFER_H
