d3d325990ed9be35dbd61c6ae69b1d442fb747e5
hustLX <1819199531@qq.com>
2023-01-14 00:35:42 +0800

    Entry point

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
+#include "GLFW/glfw3.h"
 #include "Core/Logger.h"
 #include "Core/Renderer.h"
 #include "Core/RendererApp.h"
+#include "Buffers/Shader.h"
 
 
 using namespace RendererSpace;
 
 
 int main() {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LINE_SMOOTH);
     RendererApp app;
     app.run();
 
