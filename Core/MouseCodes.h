3529e9e0e3f62bab56fd09e31fb8c0b005e8b591
hustLX <1819199531@qq.com>
2023-01-13 20:35:08 +0800

    Codes

---
+++ Core/MouseCodes.h
@@ -0,0 +1,32 @@
+// @author: lx
+// @time: 2023/1/13
+// @brief: 
+
+#pragma once
+#ifndef GAME_MOUSECODES_H
+#define GAME_MOUSECODES_H
+
+#include "Core.h"
+
+namespace RendererSpace {
+    using MouseCode = uint16_t;
+
+    namespace Mouse {
+        enum : MouseCode {
+            Button0                = 0,
+            Button1                = 1,
+            Button2                = 2,
+            Button3                = 3,
+            Button4                = 4,
+            Button5                = 5,
+            Button6                = 6,
+            Button7                = 7,
+            ButtonLast             = Button7,
+            ButtonLeft             = Button0,
+            ButtonRight            = Button1,
+            ButtonMiddle           = Button2
+        };
+    }
+}
+
+#endif //GAME_MOUSECODES_H
