fb247c7203bc329e2a1a3eb87e8477231bd5d0e8
hustLX <1819199531@qq.com>
2023-01-13 20:37:01 +0800

    Add OpenGL event callback

--- Platform/OpenGL/OpenGLWindow.cpp
+++ Platform/OpenGL/OpenGLWindow.cpp
@@ -1,4 +1,7 @@
 #include "OpenGLWindow.h"
+#include "Events/KeyEvent.h"
+#include "Events/MouseEvent.h"
+#include "Events/ApplicationEvent.h"
 
 
 bool RendererSpace::OpenGLWindow::s_glfwInit = false;
@@ -28,6 +31,11 @@ RendererSpace::OpenGLWindow::OpenGLWindow(const RendererSpace::WindowProps &prop
     glfwSetWindowUserPointer(m_window, &m_windowData);
     setVSync(true);
 
+    setGLFWWindowEventCallback();
+    setGLFWMouseEventCallback();
+    setGLFWKeyEventCallback();
+    setGLFWApplicationEventCallback();
+
     LOG_INFO("createWindow window \"{0}\": ({1}, {2})", m_windowData.Title, m_windowData.Width, m_windowData.Height);
 }
 
@@ -35,3 +43,119 @@ void RendererSpace::OpenGLWindow::onUpdate() {
     glfwPollEvents();
     m_context->swapBuffers();
 }
+
+void RendererSpace::OpenGLWindow::setGLFWWindowEventCallback() {
+    // set glfw window resize callback
+    glfwSetWindowSizeCallback(m_window, [](GLFWwindow* window, int width, int height){
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        WindowResizeEvent event(width, height);
+        data.Width = width;
+        data.Height = height;
+        data.eventCallback(event);  // call WindowsWindow callback function
+    });
+
+    // set glfw window close callback
+    glfwSetWindowCloseCallback(m_window, [](GLFWwindow* window) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        WindowCloseEvent event;
+        data.eventCallback(event);  // call WindowsWindow callback function
+    });
+
+    // set glfw window move callback
+    glfwSetWindowPosCallback(m_window, [](GLFWwindow* window, int xPos, int yPos) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        WindowMovedEvent event(xPos, yPos);
+        data.eventCallback(event);
+    });
+
+
+    glfwSetWindowFocusCallback(m_window, [](GLFWwindow* window, int focused) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+        switch (focused) {
+            case GLFW_TRUE: {
+                WindowFocusEvent event;
+                data.eventCallback(event);
+                break;
+            }
+            case GLFW_FALSE: {
+                WindowLostFocusEvent event;
+                data.eventCallback(event);
+                break;
+            }
+        }
+    });
+}
+
+void RendererSpace::OpenGLWindow::setGLFWMouseEventCallback() {
+    // set glfw mouse button callback
+    glfwSetMouseButtonCallback(m_window, [](GLFWwindow* window, int button, int action, int mods) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        switch (action) {
+            case GLFW_PRESS: {
+                MouseButtonPressedEvent event(button);
+                data.eventCallback(event);
+                break;
+            }
+            case GLFW_RELEASE: {
+                MouseButtonReleasedEvent event(button);
+                data.eventCallback(event);
+                break;
+            }
+        }
+    });
+
+    // set glfw scroll event callback
+    glfwSetScrollCallback(m_window, [](GLFWwindow* window, double xOffset, double yOffset) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        MouseScrolledEvent event((float)xOffset, (float)yOffset);
+        data.eventCallback(event);
+    });
+
+    // set glfw mouse move event callback
+    glfwSetCursorPosCallback(m_window, [](GLFWwindow* window, double xPos, double yPos) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        MouseMovedEvent event((float)xPos, (float)yPos);
+        data.eventCallback(event);
+    });
+}
+
+void RendererSpace::OpenGLWindow::setGLFWKeyEventCallback() {
+    // set glfw key events callback
+    glfwSetKeyCallback(m_window,[](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+        // press release and repeat
+        switch(action) {
+            case GLFW_PRESS: {
+                KeyPressedEvent event(key, false);
+                data.eventCallback(event);
+                break;
+            }
+                break;
+            case GLFW_RELEASE: {
+                KeyReleasedEvent event(key);  // call WindowsWindow callback
+                data.eventCallback(event);  // call WindowsWindow callback function
+                break;
+            }
+            case GLFW_REPEAT: {
+                KeyPressedEvent event(key, true);
+                data.eventCallback(event);
+                break;
+            }
+        }
+    });
+}
+
+void RendererSpace::OpenGLWindow::setGLFWApplicationEventCallback() {
+
+}
+
+void RendererSpace::OpenGLWindow::setVSync(bool enable) {
+    if(enable)
+        glfwSwapInterval(1);
+    else
+        glfwSwapInterval(0);
+    m_windowData.bVSync = enable;
+}
+
+
--- Platform/OpenGL/OpenGLWindow.h
+++ Platform/OpenGL/OpenGLWindow.h
@@ -11,17 +11,14 @@
 
 
 namespace RendererSpace {
-    class OpenGLWindow: public Window{
+    class OpenGLWindow: public Window {
     public:
         OpenGLWindow(const WindowProps& props);
+        ~OpenGLWindow() { shutDown(); }
 
-        unsigned int getWidth() const override {
-            return m_windowData.Width;
-        }
+        unsigned int getWidth() const override { return m_windowData.Width; }
 
-        unsigned int getHeight() const override {
-            return m_windowData.Height;
-        }
+        unsigned int getHeight() const override { return m_windowData.Height; }
 
         const std::string & getWindowName() const override {
             return m_windowData.Title;
@@ -33,13 +30,25 @@ namespace RendererSpace {
             return m_windowData.bVSync;
         }
 
-        void setVSync(bool enable) override final {
-            m_windowData.bVSync = true;
-        }
+        void setVSync(bool enable) override final;
 
         void* getNativeWindow() const override {
             return m_window;
         }
+
+        void shutDown() override { glfwDestroyWindow(m_window); }
+
+        void setEventCallback(const RendererSpace::Window::EventCallbackFun &callback) override {
+            m_windowData.eventCallback = callback;
+        }
+
+        void setGLFWWindowEventCallback();
+
+        void setGLFWMouseEventCallback();
+
+        void setGLFWKeyEventCallback();
+
+        void setGLFWApplicationEventCallback();
 
 
     private:
@@ -49,6 +58,8 @@ namespace RendererSpace {
             std::string Title;
             unsigned Width, Height;
             bool bVSync;
+
+            EventCallbackFun eventCallback;
         };
 
         WindowData m_windowData;
