1c72be7fa20606afbe9c8ffb9b247b0883f862cb
hustLX <1819199531@qq.com>
2023-01-15 05:24:25 +0800

    Assets

--- Assets/Shaders/Renderer2D_Triangle.glsl
+++ Assets/Shaders/Renderer2D_Triangle.glsl
@@ -4,58 +4,75 @@
 #version 450 core
 
 layout(location = 0) in vec3 a_Position;
-layout(location = 1) in vec4 a_Color;
-layout(location = 2) in vec2 a_TexCoord;
-layout(location = 3) in float a_TexIndex;
-layout(location = 4) in float a_TilingFactor;
-layout(location = 5) in int a_EntityID;
+layout(location = 1) in vec3 a_Normal;
+layout(location = 2) in vec4 a_Color;
+layout(location = 3) in vec2 a_TexCoord;
+layout(location = 4) in float a_TexIndex;
 
-layout(std140, binding = 0) uniform Camera
-{
-    mat4 u_ViewProjection;
-};
+uniform mat4 u_ViewProjection;
 
-struct VertexOutput
-{
-    vec4 Color;
-    vec2 TexCoord;
-    float TilingFactor;
-};
-
-layout (location = 0) out VertexOutput Output;
-layout (location = 3) out flat float v_TexIndex;
-layout (location = 4) out flat int v_EntityID;
+layout(location = 0) out vec4 o_Color;
+layout(location = 1) out vec2 o_TexCoord;
+layout(location = 2) out float o_TexIndex;
 
 void main()
 {
-    Output.Color = a_Color;
-    Output.TexCoord = a_TexCoord;
-    Output.TilingFactor = a_TilingFactor;
-    v_TexIndex = a_TexIndex;
-    v_EntityID = a_EntityID;
+    o_Color = a_Color;
+//    o_Color = vec4(clamp(a_Position, 0., 1.), 1.);
+    o_TexCoord = a_TexCoord;
+    o_TexIndex = a_TexIndex;
     gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
+
 
 #type fragment
 #version 450 core
 
 layout(location = 0) out vec4 o_Color;
-layout(location = 1) out int o_EntityID;
 
-struct VertexOutput
-{
-    vec4 Color;
-    vec2 TexCoord;
-    float TilingFactor;
-};
+layout(binding = 0) uniform sampler2D u_Textures[32];
 
-layout(location = 0) in VertexOutput Input;
-layout(location = 3) in flat float v_TexIndex;
-layout(location = 4) in flat int v_EntityID;
+layout(location = 0) in vec4 v_Color;
+layout(location = 1) in vec2 v_TexCoord;
+layout(location = 2) flat in float v_TexIndex;  // use flat to avoid interpolation
 
 void main()
 {
-    vec4 texColor = Input.Color;
+    vec4 texColor;
+    switch(int(v_TexIndex)) {
+        case -1: texColor = v_Color;                             break;
+        case  0: texColor = texture(u_Textures[ 0], v_TexCoord); break;
+        case  1: texColor = texture(u_Textures[ 1], v_TexCoord); break;
+        case  2: texColor = texture(u_Textures[ 2], v_TexCoord); break;
+        case  3: texColor = texture(u_Textures[ 3], v_TexCoord); break;
+        case  4: texColor = texture(u_Textures[ 4], v_TexCoord); break;
+        case  5: texColor = texture(u_Textures[ 5], v_TexCoord); break;
+        case  6: texColor = texture(u_Textures[ 6], v_TexCoord); break;
+        case  7: texColor = texture(u_Textures[ 7], v_TexCoord); break;
+        case  8: texColor = texture(u_Textures[ 8], v_TexCoord); break;
+        case  9: texColor = texture(u_Textures[ 9], v_TexCoord); break;
+        case 10: texColor = texture(u_Textures[10], v_TexCoord); break;
+        case 11: texColor = texture(u_Textures[11], v_TexCoord); break;
+        case 12: texColor = texture(u_Textures[12], v_TexCoord); break;
+        case 13: texColor = texture(u_Textures[13], v_TexCoord); break;
+        case 14: texColor = texture(u_Textures[14], v_TexCoord); break;
+        case 15: texColor = texture(u_Textures[15], v_TexCoord); break;
+        case 16: texColor = texture(u_Textures[16], v_TexCoord); break;
+        case 17: texColor = texture(u_Textures[17], v_TexCoord); break;
+        case 18: texColor = texture(u_Textures[18], v_TexCoord); break;
+        case 19: texColor = texture(u_Textures[19], v_TexCoord); break;
+        case 20: texColor = texture(u_Textures[20], v_TexCoord); break;
+        case 21: texColor = texture(u_Textures[21], v_TexCoord); break;
+        case 22: texColor = texture(u_Textures[22], v_TexCoord); break;
+        case 23: texColor = texture(u_Textures[23], v_TexCoord); break;
+        case 24: texColor = texture(u_Textures[24], v_TexCoord); break;
+        case 25: texColor = texture(u_Textures[25], v_TexCoord); break;
+        case 26: texColor = texture(u_Textures[26], v_TexCoord); break;
+        case 27: texColor = texture(u_Textures[27], v_TexCoord); break;
+        case 28: texColor = texture(u_Textures[28], v_TexCoord); break;
+        case 29: texColor = texture(u_Textures[29], v_TexCoord); break;
+        case 30: texColor = texture(u_Textures[30], v_TexCoord); break;
+        case 31: texColor = texture(u_Textures[31], v_TexCoord); break;
+    }
     o_Color = texColor;
-    o_EntityID = v_EntityID;
 }
