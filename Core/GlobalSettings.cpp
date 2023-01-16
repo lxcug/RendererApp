33590d739b0eef0fb6eaecac21e0616803291b95
hustLX <1819199531@qq.com>
2023-01-16 21:05:55 +0800

    Add Global Settings

---
+++ Core/GlobalSettings.cpp
@@ -0,0 +1,11 @@
+#include "GlobalSettings.h"
+
+
+namespace RendererSpace {
+    bool GlobalSettings::b_useFrameBuffer = true;
+    bool GlobalSettings::b_useDepthBuffer = true;
+    bool GlobalSettings::b_enableBlend = true;
+
+    bool GlobalSettings::b_enableImGuiWidgetSettings = true;
+    bool GlobalSettings::b_enableImGuiWidgetStatistic = true;
+}
---
+++ Core/GlobalSettings.h
@@ -0,0 +1,47 @@
+// @author: lx
+// @time: 2023/1/16
+// @brief: 
+
+#ifndef RENDERER_GLOBALSETTINGS_H
+#define RENDERER_GLOBALSETTINGS_H
+
+
+namespace RendererSpace {
+    struct SettingType {
+        bool SettingOption;
+        bool NeedRestart;
+    };
+}
+
+
+namespace RendererSpace {
+    class GlobalSettings {
+    public:
+        static void init() {
+            loadSettingsFromYaml();
+            onUpdate();
+        }
+
+        // TODO load from yaml file and commit changes to the file
+        static void loadSettingsFromYaml() {}
+        static void changeSettings() { }
+
+        static void onUpdate() {
+            // TODO for some settings need to restart app, use onUpdate to reinitialize some class to implement the same effect
+            // TODO setting relevant to OpenGL need to restart
+        }
+
+        // OpenGL Settings
+        static bool b_enableBlend;
+        static bool b_useFrameBuffer;
+        static bool b_useDepthBuffer;
+
+
+        // ImGui Settings
+        static bool b_enableImGuiWidgetSettings;
+        static bool b_enableImGuiWidgetStatistic;
+    };
+}
+
+
+#endif //RENDERER_GLOBALSETTINGS_H
